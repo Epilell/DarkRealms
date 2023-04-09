c91bf4d991cafcbb5b8e9e37d47b836f2e1e5bc2
Epilell <119926264+Epilell@users.noreply.github.com>
2023-04-09 17:28:46 +0900

    M2
    M2

--- Assets/Mob/Scripts/MobMove.cs
+++ Assets/Mob/Scripts/MobMove.cs
@@ -8,7 +8,7 @@ public class MobMove : MonoBehaviour
     private int xSpeed = 0;
     private int ySpeed = 0;
     public int MaxSpeed = 1;
-    int randomMoveTime;
+    //int randomMoveTime;
     Animator anim;
     SpriteRenderer spriteRenderer;
     void Awake()
@@ -16,7 +16,7 @@ public class MobMove : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-        Think();
+        InvokeRepeating("Think", 3f,3f);
     }
 
     // Update is called once per frame
@@ -24,30 +24,24 @@ public class MobMove : MonoBehaviour
     {
         rigid.velocity = new Vector2(xSpeed, ySpeed);
     }
-    //위치를 랜덤으로 이동, 3~7초안에 재귀적으로 호출
+    //위치를 랜덤으로 이동 재귀적으로 호출
     void Think()
     {
         xSpeed = Random.Range(-MaxSpeed, MaxSpeed+1);
         ySpeed = Random.Range(-MaxSpeed, MaxSpeed+1);
         int speed = 1;
-        if (ySpeed == 0)
+        if (ySpeed == 0&& xSpeed == 0)
         {
-            if(xSpeed == 0)
-            {
                 speed = 0;
-            }
         }
         anim.SetInteger("WalkSpeed",speed);
         //뒤집기
-        if (speed != 0)
+        if (speed != 0&& xSpeed > 0)
         {
-            if (xSpeed > 0)
-            {
                 spriteRenderer.flipX = true;
-            }
         }
         //랜덤타임 뒤 재귀
-        randomMoveTime = Random.Range(3, 8);
-        Invoke("Think()", randomMoveTime);
+        //randomMoveTime = Random.Range(3, 8);
+        //Invoke("Think", 3);
     }
 }
