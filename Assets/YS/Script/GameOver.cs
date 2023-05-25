e425138585f33c0c6eb048d34714413eeda8f67e
duys2 <minigun5@naver.com>
2023-05-25 13:38:39 +0900

    0525

--- Assets/YS/Script/GameOver.cs
+++ Assets/YS/Script/GameOver.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 
 public class GameOver : MonoBehaviour
 {
-    public OldInventory oldInventory; // 인벤토리 적용한 객체(여기서는 플레이어)
-    public Player player; // 플레이어 객체
-    private float playTime = 10f; // 지금은 10초지만 나중에 10분으로 바꾸면 됨
+    public OldInventory oldInventory;
+    public Player player;
+    private float playTime = 10f; // 테스트용 10초
 
     private void Start() { StartCoroutine(GameOverRoutine()); }
 
@@ -13,22 +13,23 @@ public class GameOver : MonoBehaviour
     {
         yield return new WaitForSeconds(playTime); // 플레이 시간만큼 지나면 아래 코드 실행
 
-        oldInventory.RemoveAllItem(); // 인벤토리의 모든 아이템 제거
+        player.CurrentHp = 0; // 죽음: 체력 0
+        FindObjectOfType<HealthBar>().ChangeHP(); // 체력바 변경
 
-        // 게임 오버
-        player.CurrentHp = 0;
-        FindObjectOfType<HealthBar>().ChangeHP();
+        oldInventory.RemoveAllItem(); // 죽으면 인벤토리 내 모든 아이템 제거
 
+        // 3초 간 정지 후 페이드 아웃
         Time.timeScale = 0;
 
         int count = 0;
-        while (count < 1800) // 3초 간 정지 후 페이드 아웃
+        while (count < 1800)
         {
             count++;
             yield return null; // 없으면 정지 안 함
         }
 
-        Time.timeScale = 1f; // 시간 다시 흐름
+        // 시간 다시 흐르며 페이드 아웃
+        Time.timeScale = 1f;
 
         FindObjectOfType<FadeOut>().Fade(); // FadeOut 클래스의 인스턴스를 찾아 Fade() 호출
     }
