3bda75f355893d6278b461ce85a4821bd4bd05b5
Epilell <119926264+Epilell@users.noreply.github.com>
2023-06-02 18:36:09 +0900

    boss1.1

--- Assets/Mob/Scripts/OpenCloseUI.cs
+++ Assets/Mob/Scripts/OpenCloseUI.cs
@@ -8,30 +8,28 @@ public class OpenCloseUI : MonoBehaviour
     public GameObject inven;
     public GameObject warehouse;
     [Header("Buttons")]
-    public Button InvenOpenButton;
     public Button InvenCloseButton;
     public Button WarehouseOpenButton;
     public Button WarehouseCloseButton;
     [Header("Tester")]
-    public GameObject Obj3;
+    public GameObject Tester;
     void Start()
     {
         //closeButton = GetComponent<Button>(); // 버튼을 참조합니다
-        InvenCloseButton.onClick.AddListener(CloseInvenUI)// 버튼 클릭 시 ClosePanel 함수를 호출합니다
+        InvenCloseButton.onClick.AddListener(CloseInvenUI);// 버튼 클릭 시 ClosePanel 함수를 호출합니다
         WarehouseCloseButton.onClick.AddListener(CloseWarehouseUI); // 버튼 클릭 시 ClosePanel 함수를 호출합니다
-        OpenButton1.onClick.AddListener(OpenUI1);
-        WarehouseOpenButton.onClick.AddListener(OpenUI2);
+        WarehouseOpenButton.onClick.AddListener(OpenWarehouseUI);
     }
     private void Update()
     {
         // 탭 키를 눌렀을 때 인벤토리를 엽니다.
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            Obj1.SetActive(!Obj1.activeSelf);
+            inven.SetActive(!inven.activeSelf);
         }
         if (Input.GetKeyDown(KeyCode.T))
         {
-            Obj3.SetActive(!Obj3.activeSelf);
+            Tester.SetActive(!Tester.activeSelf);
         }
     }
     public void CloseInvenUI()
@@ -42,11 +40,7 @@ public class OpenCloseUI : MonoBehaviour
     {
         warehouse.SetActive(false); // UI 오브젝트 비활성화
     }
-    public void OpenUI1()
-    {
-        Obj1.SetActive(true);
-    }
-    public void OpenWarehouseUI2()
+    public void OpenWarehouseUI()
     {
         warehouse.SetActive(true);
     }
