000d037bb65b0df87db52fb53e9aa2781038e6b6
jju9704 <128231052+jju9704@users.noreply.github.com>
2023-06-07 22:31:31 +0900

    DDO DDO Debug
    고쳤다

--- Assets/Player/Script/Weapon/WeaponManager.cs
+++ Assets/Player/Script/Weapon/WeaponManager.cs
@@ -3,6 +3,7 @@ using Rito.InventorySystem;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class WeaponManager : MonoBehaviour
@@ -32,18 +33,18 @@ public class WeaponManager : MonoBehaviour
     /// 인벤토리에서 무기를 변경하는 경우
     /// </summary>
     /// <param name="weaponName"></param>
-    private void ChangeWeapon(string weaponName)
+    private void ChangeWeapon(Rito.InventorySystem.WeaponItemData data)
     {
         if (this.transform.childCount >= 1)
         {
             RemoveWeapon();
         }
 
-        curWeaponName = weaponName;
+        curWeaponName = data.Name;
 
-        GameObject prefab = Resources.Load<GameObject>("Prefabs/" + weaponName);
+        GameObject prefab = Resources.Load<GameObject>("Prefabs/" + RemoveNumber(data.Name));
         GameObject Instance = Instantiate(prefab, this.transform);
-        Instance.name = weaponName;
+        Instance.name = data.Name;
         Instance.GetComponent<WeaponBase>().SetStatus(EquipmentInventory.instance.EqItems[2].Data as WeaponItemData);
     }
 
@@ -76,43 +77,17 @@ public class WeaponManager : MonoBehaviour
 
             Destroy(child.gameObject);
         }
+
+        curWeaponName = null;
     }
     #endregion
 
     //Check Method
     #region
 
-    /// <summary>
-    /// 현재 장착한 무기의 종류를 확인하여 리턴
-    /// </summary>
-    /// <param name="data"></param>
-    /// <returns></returns>
-    private int CheckCurWeapon(Rito.InventorySystem.ItemData data)
+    private string RemoveNumber(string name)
     {
-        if(data == null)
-        {
-            return 0;
-        }
-        else
-        {
-            string s1 = data.name;
-            if (s1.Contains("rifle"))
-            {
-                return 1;
-            }
-            else if (s1.Contains("shotgun"))
-            {
-                return 2;
-            }
-            else if (s1.Contains("pistol"))
-            {
-                return 3;
-            }
-            else
-            {
-                return 0;
-            }
-        }
+        return Regex.Replace(name, @"[^a-zA-Z]", "");
     }
 
     #endregion
@@ -130,117 +105,55 @@ public class WeaponManager : MonoBehaviour
         // 장비 인벤토리가 있으면 ( 실제 적용 )
         if (EquipmentInventory.instance != null)
         {
-            Debug.Log("a");
+            //Debug.Log("장비 인벤토리 있음");
             // 1. 시즈모드가 켜져있다면
             if (SkillManager.Instance.siegemodedata.IsActive)
             {
-                Debug.Log("1");
+                //Debug.Log("시즈모드 ON");
                 // 1-1. 현재 무기가 없거나 현재 무기가 dual pistol이 아니면
                 if (curWeaponName == null || curWeaponName != "dual pistol")
                 {
+                    //Debug.Log("무기 바꿈");
                     ChangeSpecialWeapon("dual pistol");
-                    Debug.Log("1-1");
                 }
             }
             else // 2. 시즈모드가 켜져있지 않으면
             {
-                Debug.Log("2");
+                //Debug.Log("시즈모드 OFF");
                 // 2-1. 현재 손에 무기가 없다면 (지속)
                 if (curWeaponName == null)
                 {
-                    Debug.Log("2-1");
+                    //Debug.Log("손에 무기 없음");
                     // 2-1-1. 인벤토리에도 무기가 없다면
                     if (EquipmentInventory.instance.EqItems[2] == null)
                     {
-                        Debug.Log("There's no weapon in Equipment");
+                        //Debug.Log("장비창에 무기없음");
                     }
                     // 2-1-2. 인벤토리에 무기가 있다면
                     else
                     {
-                        Debug.Log("2-1-2");
+                        //Debug.Log("무기 바꿈");
                         //현재 인벤토리 무기를 확인하여 변경
-                        switch (CheckCurWeapon(EquipmentInventory.instance.EqItems[2].Data))
-                        {
-                            case 0:
-                                curWeaponName = null;
-                                RemoveWeapon();
-                                break;
-                            case 1:
-                                ChangeWeapon("rifle");
-                                break;
-                            case 2:
-                                ChangeWeapon("shotgun");
-                                break;
-                            case 3:
-                                ChangeWeapon("pistol");
-                                break;
-                        }
+                        ChangeWeapon(EquipmentInventory.instance.EqItems[2].Data as WeaponItemData);
                     }
                 }
                 // 2-2. 현재 손에 무기가 있다면
                 else
                 {
-                    Debug.Log("2-2");
+                    //Debug.Log("손에 무기있음");
                     // 2-2-1. 인벤토리에는 무기가 없다면
                     if (EquipmentInventory.instance.EqItems[2] == null)
                     {
-                        Debug.Log("2-2-1");
-                        curWeaponName = null;
+                        //Debug.Log("무기 없앰");
                         RemoveWeapon();
                     }
                     // 2-2-2. 인벤토리에 무기가 있는데 다른 무기라면
                     else if (curWeaponName != EquipmentInventory.instance.EqItems[2].Data.Name)
                     {
-                        Debug.Log("2-2-2");
+                        //Debug.Log("무기 바꿈");
                         //현재 인벤토리 무기를 확인하여 변경
-                        switch (CheckCurWeapon(EquipmentInventory.instance.EqItems[2].Data))
-                        {
-                            case 0:
-                                curWeaponName = null;
-                                RemoveWeapon();
-                                break;
-                            case 1:
-                                ChangeWeapon("rifle");
-                                break;
-                            case 2:
-                                ChangeWeapon("shotgun");
-                                break;
-                            case 3:
-                                ChangeWeapon("pistol");
-                                break;
-                        }
+                        ChangeWeapon(EquipmentInventory.instance.EqItems[2].Data as WeaponItemData);
                     }
-                }
-            }
-        }
-        //장비 인벤토리가 존재하는 경우 ( 테스트용 )
-        else
-        {
-            Debug.Log("b");
-            if (SkillManager.Instance.siegemodedata.IsActive)
-            {
-                Debug.Log("1");
-                // 1-1. 현재 무기가 없거나 현재 무기가 dual pistol이 아니면
-                if (curWeaponName == null || curWeaponName != "dual pistol")
-                {
-                    ChangeSpecialWeapon("dual pistol");
-                    Debug.Log("1-1");
-                }
-            }
-            else // 2. 시즈모드가 켜져있지 않으면
-            {
-                Debug.Log("2");
-                // 2-1. 현재 손에 무기가 없다면 (지속)
-                if (curWeaponName == null)
-                {
-                    Debug.Log("2-1");
-                }
-                // 2-2. 현재 손에 무기가 있다면
-                else
-                {
-                    Debug.Log("2-2");
-                    curWeaponName = null;
-                    RemoveWeapon();
                 }
             }
         }
