e698875b515632f20941a18d0d97e730360a704d
jju9704 <128231052+jju9704@users.noreply.github.com>
2023-06-07 21:39:53 +0900

    DDo Debug

---
+++ Assets/Player/Script/System/PlayerSpawnData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Spawn Point", menuName = "Map Data/Map Spawn Point Data")]
+public class PlayerSpawnData : ScriptableObject
+{
+    private string mapname;
+    public string Mapname => mapname;
+
+    public List<Vector2> SpawnPoints = new List<Vector2>();
+}
--- Assets/Player/Script/System/PlayerSpawnSystem.cs
+++ Assets/Player/Script/System/PlayerSpawnSystem.cs
@@ -2,15 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Spawn Point", menuName = "Map Data/Map Spawn Point Data")]
-public class PlayerSpawnData: ScriptableObject
-{
-    private string mapname;
-    public string Mapname => mapname;
-
-    public List<Vector2> SpawnPoints = new List<Vector2>();
-}
-
 public class PlayerSpawnSystem : MonoBehaviour
 {
     public static PlayerSpawnSystem instance;
