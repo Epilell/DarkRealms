80730739babfbe7c2cf6858aa89b4463dc833969
duys2 <minigun5@naver.com>
2023-08-25 09:25:47 +0900

    0825_2
    MainMenu 페이드 아웃 수정

--- Assets/YS/Script/FadeOut.cs
+++ Assets/YS/Script/FadeOut.cs
@@ -6,7 +6,7 @@ using UnityEditor;
 
 public class FadeOut : MonoBehaviour
 {
-    public Image panel; // 페이드 아웃에 사용할 이미지
+    public Image panel, startImg; // 페이드 아웃에 사용할 이미지
     float time = 0f; // 경과 시간
     float fadeTime = 1f; // 페이드 아웃 시간
     public SceneAsset scene; // 씬 가져오기
@@ -36,7 +36,17 @@ public class FadeOut : MonoBehaviour
         }
         else // 메인씬에서 Start 버튼을 눌렀을 때
         {
-            yield return new WaitForSeconds(1f); // 1초 대기
+            startImg.gameObject.SetActive(true); // 이미지 활성화
+            Color alpha = startImg.color; // 색상값
+
+            // 페이드 아웃
+            while (alpha.a < fadeTime)
+            {
+                time += Time.deltaTime / fadeTime; // 시간 계산
+                alpha.a = Mathf.Lerp(0f, 1f, time); // 알파값을 서서히 증가시킴(서서히 어두워짐)
+                startImg.color = alpha; // 이미지의 색상 정보변경
+                yield return null; // 한 프레임 대기
+            }
 
             SceneManager.LoadScene(scene.name); // 씬 전환
         }
