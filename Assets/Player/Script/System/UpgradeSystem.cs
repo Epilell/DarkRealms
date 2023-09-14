514b616f4f4dfe64b4bf7050b5f415ff841e2e59
Hing <128231052+jju9704@users.noreply.github.com>
2023-09-14 22:21:30 +0900

    0914
    강화 시스템 개선

--- Assets/Player/Script/System/UpgradeSystem.cs
+++ Assets/Player/Script/System/UpgradeSystem.cs
@@ -15,14 +15,15 @@ namespace Rito.InventorySystem
         //Field
         #region .
         // 장비 아이템과 인벤토리 아이템
-        [SerializeField] private InvenData EquipmentInventory;
+        [SerializeField] private EquipmentInventory EquipmentInventory;
         [SerializeField] private Inventory inventory;
 
+        //업그레이드 표시 영역
         [SerializeField] private RectTransform contentArea;
 
         // 변경된 인벤토리랑 비교
-        private List<ItemData> latestEquipmentItemList;
-        private List<Item> latestItemList;
+        [SerializeField] private List<Item> latestEquipmentItemList;
+        [SerializeField] private List<Item> latestItemList;
 
         // 업그레이드 패널 프리팹
         [SerializeField] private GameObject targetPanelPrefab;
@@ -55,32 +56,6 @@ namespace Rito.InventorySystem
             }
             return true;
         }
-
-        /// <summary>
-        /// 리스트에 변화가 있는지 리턴 ( 같으면 T 다르면 F )
-        /// </summary>
-        /// <param name="list1">ItemData</param>
-        /// <param name="list2">ItemData</param>
-        /// <returns></returns>
-        bool AreListsEqual(List<ItemData> list1, List<ItemData> list2)
-        {
-            if (list1.Count != list2.Count)
-                return false;
-
-            for (int i = 0; i < list1.Count; i++)
-            {
-                if (list1[i] == null || list2[i] == null)
-                {
-                    if (list1[i] != list2[i])
-                        return false;
-                }
-                else if (!list1[i].Equals(list2[i]))
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
         #endregion
 
         //Method
@@ -89,11 +64,11 @@ namespace Rito.InventorySystem
         //업그레이드 패널 생성
         #region .
         /// <summary> 업그레이드 가능한 아이템 리스트를 가져오고 업그레이드 패널 생성 </summary>
-        /// <param name="_items"></param>
-        private void FindUpgradableItemAndMakeList(Item[] _items)
+        /// <param name="_inventoryItems"></param>
+        private void FindUpgradableItemAndMakeList(Item[] _inventoryItems, Item[] _equipmentItems)
         {
             //아이템 리스트가 비어있을경우
-            if (_items != null && _items.Length != 0)
+            if ( (_inventoryItems != null && _inventoryItems.Length != 0) || (_equipmentItems != null && _equipmentItems.Length != 0) )
             {
                 //기존의 패널 삭제
                 for (int i = contentArea.childCount - 1; i >= 0; i--)
@@ -106,13 +81,13 @@ namespace Rito.InventorySystem
                 int num = 0;
 
                 // 장비창 아이템 리스트의 길이만큼 생성
-                for (int i = 0; i < EquipmentInventory.invenitems.Count; i++)
+                for (int i = 0; i < _equipmentItems.Length; i++)
                 {
                     //리스너 기능 작동을 위한 변수
                     int index = i;
 
                     // 1. 아이템이 null이 아니고 다음 업그레이드가 있는 경우
-                    if (_items[i] != null && _items[i].HasNextItemData())
+                    if (_equipmentItems[i] != null && _equipmentItems[i].HasNextItemData() )
                     {
                         num++;
                         RectTransform RT = CloneUpgradePanel();
@@ -129,10 +104,10 @@ namespace Rito.InventorySystem
                         RT.gameObject.SetActive(true);
 
                         // 1-1. 방어구인 경우
-                        if (_items[i].GetType() == typeof(ArmorItem))
+                        if (_equipmentItems[i].GetType() == typeof(ArmorItem))
                         {
                             //해당 업그레이드 패널의 이미지 할당
-                            ArmorItemData data = _items[i].Data as ArmorItemData;
+                            ArmorItemData data = _equipmentItems[i].Data as ArmorItemData;
                             var UI = RT.GetComponent<UpgradePanelUI>();
                             UI._beforeImage.sprite = data.IconSprite;
                             UI._afterImage.sprite = data.NextArmorData.IconSprite;
@@ -148,15 +123,15 @@ namespace Rito.InventorySystem
                             //해당 패널의 업그레이드 버튼에 기능 할당
                             UI._upgradeButton.onClick.AddListener(() =>
                             {
-                                AttemptDirectArmorUpgrade(data, index);
+                                AttemptArmorUpgrade(EquipmentInventory, data, index);
                             });
                         }
 
                         // 1-2. 무기인 경우
-                        else if (_items[i].GetType() == typeof(WeaponItem))
+                        else if (_equipmentItems[i].GetType() == typeof(WeaponItem))
                         {
                             //해당 업그레이드 패널의 이미지 할당
-                            WeaponItemData data = (WeaponItemData)_items[i].Data;
+                            WeaponItemData data = _equipmentItems[i].Data as WeaponItemData;
                             UpgradePanelUI UI = RT.GetComponent<UpgradePanelUI>();
                             UI._beforeImage.sprite = data.IconSprite;
                             UI._afterImage.sprite = data.NextWeaponData.IconSprite;
@@ -172,7 +147,7 @@ namespace Rito.InventorySystem
                             //해당 패널의 업그레이드 버튼에 기능 할당
                             UI._upgradeButton.onClick.AddListener(() =>
                             {
-                                AttemptDirectWeaponUpgrade(data, index);
+                                AttemptWeaponUpgrade(EquipmentInventory, data, index);
                             });
                         }
                     }
@@ -188,13 +163,13 @@ namespace Rito.InventorySystem
                 }
 
                 // 인벤토리 아이템 리스트의 길이만큼 생성
-                for (int i = 0; i < _items.Length; i++)
+                for (int j = 0; j < _inventoryItems.Length; j++)
                 {
                     //리스너 기능 작동을 위한 변수
-                    int index = i;
+                    int index = j;
 
                     // 1. 아이템이 null이 아니고 다음 업그레이드가 있는 경우
-                    if (_items[i] != null && _items[i].HasNextItemData())
+                    if (_inventoryItems[j] != null && _inventoryItems[j].HasNextItemData())
                     {
                         num++;
                         RectTransform RT = CloneUpgradePanel();
@@ -211,10 +186,10 @@ namespace Rito.InventorySystem
                         RT.gameObject.SetActive(true);
 
                         // 1-1. 방어구인 경우
-                        if (_items[i].GetType() == typeof(ArmorItem))
+                        if (_inventoryItems[j].GetType() == typeof(ArmorItem))
                         {
                             //해당 업그레이드 패널의 이미지 할당
-                            ArmorItemData data = _items[i].Data as ArmorItemData;
+                            ArmorItemData data = _inventoryItems[j].Data as ArmorItemData;
                             var UI = RT.GetComponent<UpgradePanelUI>();
                             UI._beforeImage.sprite = data.IconSprite;
                             UI._afterImage.sprite = data.NextArmorData.IconSprite;
@@ -230,15 +205,15 @@ namespace Rito.InventorySystem
                             //해당 패널의 업그레이드 버튼에 기능 할당
                             UI._upgradeButton.onClick.AddListener(() =>
                             {
-                                AttemptArmorUpgrade(data, index);
+                                AttemptArmorUpgrade(inventory, data, index);
                             });
                         }
 
                         // 1-2. 무기인 경우
-                        else if (_items[i].GetType() == typeof(WeaponItem))
+                        else if (_inventoryItems[j].GetType() == typeof(WeaponItem))
                         {
                             //해당 업그레이드 패널의 이미지 할당
-                            WeaponItemData data = (WeaponItemData)_items[i].Data;
+                            WeaponItemData data = (WeaponItemData)_inventoryItems[j].Data;
                             UpgradePanelUI UI = RT.GetComponent<UpgradePanelUI>();
                             UI._beforeImage.sprite = data.IconSprite;
                             UI._afterImage.sprite = data.NextWeaponData.IconSprite;
@@ -254,7 +229,7 @@ namespace Rito.InventorySystem
                             //해당 패널의 업그레이드 버튼에 기능 할당
                             UI._upgradeButton.onClick.AddListener(() =>
                             {
-                                AttemptWeaponUpgrade(data, index);
+                                AttemptWeaponUpgrade(inventory, data, index);
                             });
                         }
                     }
@@ -280,80 +255,76 @@ namespace Rito.InventorySystem
         /// <summary> 인벤토리 방어구 아이템 강화 </summary>
         /// <param name="_data"></param>
         /// <param name="_index"></param>
-        private void AttemptArmorUpgrade(ArmorItemData _data, int _index)
+        private void AttemptArmorUpgrade(Inventory _target, ArmorItemData _data, int _index)
         {
             if (_data.Requirements.Count != 0)
             {
                 if (inventory.UseMaterial(_data.Requirements[0].Data, _data.Requirements[0].Num))
                 {
-                    inventory.Remove(_index);
-                    inventory.Add(_data.NextArmorData);
+                    _target.Remove(_index);
+                    _target.Add(_data.NextArmorData);
                 }
             }
             else
             {
-                inventory.Remove(_index);
-                inventory.Add(_data.NextArmorData);
+                _target.Remove(_index);
+                _target.Add(_data.NextArmorData);
             }
         }
 
-        /// <summary>
-        /// 장비창 방어구 아이템 강화
-        /// </summary>
-        /// <param name="_data"></param>
-        /// <param name="_index"></param>
-        private void AttemptDirectArmorUpgrade(ArmorItemData _data, int _index)
+        private void AttemptArmorUpgrade(EquipmentInventory _target, ArmorItemData _data, int _index)
         {
             if (_data.Requirements.Count != 0)
             {
                 if (inventory.UseMaterial(_data.Requirements[0].Data, _data.Requirements[0].Num))
                 {
-                    EquipmentInventory.invenitems[_index] = _data.NextArmorData;
+                    _target.RemoveItem(_index);
+                    _target.Add(_data.NextArmorData);
                 }
             }
             else
             {
-                EquipmentInventory.invenitems[_index] = _data.NextArmorData;
+                _target.RemoveItem(_index);
+                _target.Add(_data.NextArmorData);
             }
         }
 
         /// <summary> 인벤토리 무기 아이템 강화 </summary>
         /// <param name="_data"></param>
         /// <param name="_index"></param>
-        private void AttemptWeaponUpgrade(WeaponItemData _data, int _index)
+        private void AttemptWeaponUpgrade(Inventory _target, WeaponItemData _data, int _index)
         {
             if (_data.Requirements.Count != 0)
             {
                 if (inventory.UseMaterial(_data.Requirements[0].Data, _data.Requirements[0].Num))
                 {
-                    inventory.Remove(_index);
-                    inventory.Add(_data.NextWeaponData);
+                    _target.Remove(_index);
+                    _target.Add(_data.NextWeaponData);
                 }
             }
             else
             {
-                inventory.Remove(_index);
-                inventory.Add(_data.NextWeaponData);
+                _target.Remove(_index);
+                _target.Add(_data.NextWeaponData);
             }
         }
-
-        /// <summary>
-        /// 장비창 무기 아이템 업그레이드
-        /// </summary>
+        /// <summary> 인벤토리 무기 아이템 강화 </summary>
         /// <param name="_data"></param>
         /// <param name="_index"></param>
-        private void AttemptDirectWeaponUpgrade(WeaponItemData _data, int _index)
+        private void AttemptWeaponUpgrade(EquipmentInventory _target, WeaponItemData _data, int _index)
         {
             if (_data.Requirements.Count != 0)
             {
                 if (inventory.UseMaterial(_data.Requirements[0].Data, _data.Requirements[0].Num))
                 {
-                    EquipmentInventory.invenitems[_index] = _data.NextWeaponData;
+                    _target.RemoveItem(_index);
+                    _target.Add(_data.NextWeaponData);
                 }
             }
             else
             {
-                EquipmentInventory.invenitems[_index] = _data.NextWeaponData;
+                _target.RemoveItem(_index);
+                _target.Add(_data.NextWeaponData);
             }
         }
         #endregion
@@ -365,19 +336,21 @@ namespace Rito.InventorySystem
         private void Start()
         {
             inventory = GameObject.FindWithTag("GameController").GetComponentInChildren<Inventory>();
+            EquipmentInventory = inventory.EqInven;
 
             latestItemList = new List<Item>(inventory._Items);
-            latestEquipmentItemList = EquipmentInventory.invenitems;
-            FindUpgradableItemAndMakeList(inventory._Items);
+            latestEquipmentItemList = new List<Item>(EquipmentInventory.EqItems);
+            FindUpgradableItemAndMakeList(inventory._Items, EquipmentInventory.EqItems);
         }
 
         private void Update()
         {
-            if (inventory._Items != null && latestItemList != null && (!AreListsEqual(latestItemList, new List<Item>(inventory._Items)) || !AreListsEqual(latestEquipmentItemList, EquipmentInventory.invenitems) ))
+            if (inventory._Items != null && latestItemList != null && (!AreListsEqual(latestItemList, new List<Item>(inventory._Items)) || !AreListsEqual(latestEquipmentItemList, new List<Item>(EquipmentInventory.EqItems)) ))
             {
                 contentArea.sizeDelta = new Vector2(contentArea.sizeDelta.x, 0);
                 latestItemList = new List<Item>(inventory._Items);
-                FindUpgradableItemAndMakeList(inventory._Items);
+                latestEquipmentItemList = new List<Item>(EquipmentInventory.EqItems);
+                FindUpgradableItemAndMakeList(inventory._Items, EquipmentInventory.EqItems);
             }
         }
         #endregion
