39e5c202be11d986ef1d9f5b7fe33d7a0ba936c1
duys2 <minigun5@naver.com>
2023-10-02 01:27:05 +0900

    1002
    튜토리얼: A, D 키로 좌우 이동

---
+++ Assets/YS/Script/TutorialUI.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public enum btnType
+{
+    Left,
+    Right
+}
+
+public class TutorialUI : MonoBehaviour
+{
+    public btnType btnType;
+
+    public GameObject tutorialObj, mainMenuObj;
+    public CanvasGroup main, option, inventory, map;
+
+    private void Start() { if (SceneManager.GetActiveScene().name == "MainMenu") tutorialObj.SetActive(false); }
+
+    public void OnOpenClick()
+    {
+        tutorialObj.SetActive(true);
+        mainMenuObj.SetActive(false);
+        /*switch (btnType)
+        {
+            case btnType.Left:
+                if (main.alpha == 0 && option.alpha == 1 && inventory.alpha == 0 && map.alpha == 0)
+                {
+                    CanvasGroupOn(main);
+                    CanvasGroupOff(option);
+                }
+                else if (main.alpha == 0 && option.alpha == 0 && inventory.alpha == 1 && map.alpha == 0)
+                {
+                    CanvasGroupOn(option);
+                    CanvasGroupOff(inventory);
+                }
+                else if (main.alpha == 0 && option.alpha == 0 && inventory.alpha == 0 && map.alpha == 1)
+                {
+                    CanvasGroupOn(inventory);
+                    CanvasGroupOff(map);
+                }
+                else { }
+                break;
+            case btnType.Right:
+                if (main.alpha == 1 && option.alpha == 0 && inventory.alpha == 0 && map.alpha == 0)
+                {
+                    CanvasGroupOn(option);
+                    CanvasGroupOff(main);
+                }
+                else if (main.alpha == 0 && option.alpha == 1 && inventory.alpha == 0 && map.alpha == 0)
+                {
+                    CanvasGroupOn(inventory);
+                    CanvasGroupOff(option);
+                }
+                else if (main.alpha == 0 && option.alpha == 0 && inventory.alpha == 1 && map.alpha == 0)
+                {
+                    CanvasGroupOn(map);
+                    CanvasGroupOff(inventory);
+                }
+                else { }
+                break;
+        }*/
+    }
+
+    public void OnCloseClick()
+    {
+        tutorialObj.SetActive(false);
+        mainMenuObj.SetActive(true);
+        main.alpha = 1;
+        option.alpha = 0;
+        inventory.alpha = 0;
+        map.alpha = 0;
+    }
+
+        private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                if (main.alpha == 0 && option.alpha == 1 && inventory.alpha == 0 && map.alpha == 0)
+                {
+                    CanvasGroupOn(main);
+                    CanvasGroupOff(option);
+                }
+                else if (main.alpha == 0 && option.alpha == 0 && inventory.alpha == 1 && map.alpha == 0)
+                {
+                    CanvasGroupOn(option);
+                    CanvasGroupOff(inventory);
+                }
+                else if (main.alpha == 0 && option.alpha == 0 && inventory.alpha == 0 && map.alpha == 1)
+                {
+                    CanvasGroupOn(inventory);
+                    CanvasGroupOff(map);
+                }
+                else { }
+            }
+
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+
+                if (main.alpha == 1 && option.alpha == 0 && inventory.alpha == 0 && map.alpha == 0)
+                {
+                    CanvasGroupOn(option);
+                    CanvasGroupOff(main);
+                }
+                else if (main.alpha == 0 && option.alpha == 1 && inventory.alpha == 0 && map.alpha == 0)
+                {
+                    CanvasGroupOn(inventory);
+                    CanvasGroupOff(option);
+                }
+                else if (main.alpha == 0 && option.alpha == 0 && inventory.alpha == 1 && map.alpha == 0)
+                {
+                    CanvasGroupOn(map);
+                    CanvasGroupOff(inventory);
+                }
+                else { }
+            }
+        }
+    }
+
+    public void CanvasGroupOn(CanvasGroup canvasGroup)
+    {
+        canvasGroup.alpha = 1;
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    public void CanvasGroupOff(CanvasGroup canvasGroup)
+    {
+        canvasGroup.alpha = 0;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
+    }
+}
