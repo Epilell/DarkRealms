a5ea29a7f7f17d8065d826e994335d9891d2b99a
Epilell <119926264+Epilell@users.noreply.github.com>
2023-11-06 19:56:59 +0900

    1106m

---
+++ Assets/Mob/Scripts/UI/QuickSlotUI.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Rito.InventorySystem;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class QuickSlotUI : MonoBehaviour
+{
+    [SerializeField]
+    private Inventory inventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
